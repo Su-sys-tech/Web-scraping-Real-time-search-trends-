b18f489279baf1cc3e28a81e40e9ab061043650e
Su-sys-tech <121379176+Su-sys-tech@users.noreply.github.com>
2022-12-25 19:00:14 +0900

    Add files via upload

---
+++ realtime_wsc.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 25 12:36:22 2022
+
+@author: raore
+"""
+
+import requests
+from bs4 import BeautifulSoup
+import re
+import datetime
+
+dt_now = datetime.datetime.now()
+#Yahooリアルタイム検索のURL
+URL = "https://search.yahoo.co.jp/realtime"
+rest = requests.get(URL)
+
+# BeautifulSoupにYahooリアルタイム検索を読み込ませる
+soup = BeautifulSoup(rest.text, "html.parser")
+
+# Yahooリアルタイム検索の見出しとURLの情報を取得して出力する
+
+print(dt_now)
+for i in range(1,3):
+    for j in range(1,11):
+        elems = str(soup.select("#atkey > section > ol:nth-child("+str(i)+") > li:nth-child("+str(j)+") > a > article > h1"))
+        new_str = re.search(r"<h1>(.+)</h1>",elems).group(1)
+        print(new_str)
+            
