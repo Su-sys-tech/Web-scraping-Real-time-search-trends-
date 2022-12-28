7d68f1c8f69a63023088c57bc630146a9177bef5
Su-sys-tech <121379176+Su-sys-tech@users.noreply.github.com>
2022-12-28 23:14:13 +0900

    Update realtime_wsc.py

--- realtime_wsc.py
+++ realtime_wsc.py
@@ -1,22 +1,33 @@
 import requests
-from bs4 import BeautifulSoup
 import re
 import datetime
+import csv
+import time
+from bs4 import BeautifulSoup
 
-dt_now = datetime.datetime.now()
-#Yahooリアルタイム検索のURL
-URL = "https://search.yahoo.co.jp/realtime"
-rest = requests.get(URL)
+def CURRENT_DATETIME():
+    dt = datetime.datetime.now()
+    current_datetime = str(str(dt.year)+"/"+str(dt.month)+"/"+str(dt.day)+" "+str(dt.hour)+":"+str(dt.minute)+":"+str(dt.second))
+    return current_datetime
 
-# BeautifulSoupにYahooリアルタイム検索を読み込ませる
-soup = BeautifulSoup(rest.text, "html.parser")
-
-# Yahooリアルタイム検索の見出しとURLの情報を取得して出力する
-
-print(dt_now)
-for i in range(1,3):
-    for j in range(1,11):
-        elems = str(soup.select("#atkey > section > ol:nth-child("+str(i)+") > li:nth-child("+str(j)+") > a > article > h1"))
-        new_str = re.search(r"<h1>(.+)</h1>",elems).group(1)
-        print(new_str)
-            
+while True:
+    trend_list = []
+    
+    #Yahooリアルタイム検索のURL
+    URL = "https://search.yahoo.co.jp/realtime"
+    rest = requests.get(URL)
+    
+    #BeautifulSoupにYahooリアルタイム検索を読み込ませる
+    soup = BeautifulSoup(rest.text, "html.parser")
+    
+    #Yahooリアルタイム検索の見出しとURLの情報を取得して出力する
+    for i in range(1,3):
+        for j in range(1,11):
+            trend_list.append(re.search(r"<h1>(.+)</h1>",str(soup.select("#atkey > section > ol:nth-child("+str(i)+") > li:nth-child("+str(j)+") > a > article > h1"))).group(1))
+    
+    with open(r"F:\realtime_trend.csv","a",newline="") as f:
+        csv_obj = csv.writer(f)
+        f.write(CURRENT_DATETIME()+",")
+        csv_obj.writerow(trend_list)
+    print(".") 
+    time.sleep(60*15)
